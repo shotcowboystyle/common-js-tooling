4d3fd759c596bb288750b09cd4c7742a2a46910c
Curtis Blanton <62471+shotcowboystyle@users.noreply.github.com>
2023-06-02 13:09:10 -0400

    fixup! fixup! fixup! feat: turbo setup with base linter configs

--- packages/prettier-config/tests/prettier.test.ts
+++ packages/prettier-config/tests/prettier.test.ts
@@ -4,7 +4,13 @@ import prettierConfig from '../src';
 describe('Prettier Config', () => {
   test('should export rules', () => {
     expect(prettierConfig.$schema).toBe('http://json.schemastore.org/prettierrc');
-    expect(prettierConfig.useTabs).toBe(true);
+    expect(prettierConfig.endOfLine).toBe('lf');
+    expect(prettierConfig.printWidth).toBe(150);
+    expect(prettierConfig.trailingComma).toBe('all');
+    expect(prettierConfig.semi).toBe(true);
+    expect(prettierConfig.tabWidth).toBe(2);
+    expect(prettierConfig.useTabs).toBe(false);
+    expect(prettierConfig.quoteProps).toBe('as-needed');
     expect(prettierConfig).toMatchSnapshot();
   });
 });
